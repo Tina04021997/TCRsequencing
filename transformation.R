4a775131b411851ac1138fc3a3548dc48627f8bb
TinaYang <60873217+Tina04021997@users.noreply.github.com>
2021-06-22 16:03:24 +0800

    Create transformation.R

---
+++ transformation.R
@@ -0,0 +1,18 @@
+install.packages("tidyverse")
+library("tidyverse")
+setwd("[Raw CSV path]")
+
+df <- read.csv("sample_name")
+df <- df[-c(1,4:5,7,9,11:22,24:36,38:39)]
+df <- df %>% slice(1:10000)
+df <- df[,c(1,2,6,7,3,4,5)]
+colnames(df) <- c("#count","freq","cdf3nt","cdr3aa","v","d","j")
+df$d <- as.character("")
+df$d <- sub("",".",as.character(df$d))
+
+df$v<- gsub("*00","",as.character(df$v))
+df$j<- gsub("*00","",as.character(df$j))
+df$v <- gsub(".{1}$", "", as.character(df$v))
+df$j <- gsub(".{1}$", "", as.character(df$j))
+
+write.csv(df, file = "sample_name.csv", row.names=FALSE)
